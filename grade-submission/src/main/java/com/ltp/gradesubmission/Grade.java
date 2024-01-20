2608faeed37d7432702c27d1e73b3c0caa68d237
Pedro-Antonetti <pedroantonetti@gmail.com>
2024-01-20 16:46:02 -0300

    Field Validation

--- grade-submission/src/main/java/com/ltp/gradesubmission/Grade.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/Grade.java
@@ -2,10 +2,15 @@ package com.ltp.gradesubmission;
 
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+
 public class Grade {
 
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @NotBlank(message = "Subject cannot be blank")
     private String subject;
+    @NotBlank(message = "Score cannot be blank")
     private String score;
     private String id;
 
